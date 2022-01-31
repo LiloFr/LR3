864e7e803ab58928d4af963d03c4e2b081d085ca
LiloFr <88384549+LiloFr@users.noreply.github.com>
2022-01-31 22:11:13 +0300

    Update lr3.py

--- lr3.py
+++ lr3.py
@@ -16,9 +16,17 @@ def x3_1(n):
   return collatz(n * 3 + 1)
 
 
-def collatz():
-  pass
+def collatz(n):
+  num = [n]
+  if n == 1:
+    pass
+  elif n % 2 == 0:
+    num.extend(x2(n))
+  else:
+    num.extend(x3_1(n))
+  return num
 
 
 if __name__ == '__main__':
-  pass
+  x = input_f()
+  print(collatz(x))
